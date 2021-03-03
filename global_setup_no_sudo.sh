37bc9169284004d061d5fbb75dbe5fda4dc16c15
AleksiJarventausta <aksu.j@hotmail.com>
2021-03-03 13:42:27 +0200

    remove global_setup.sh from flow.

--- global_setup_no_sudo.sh
+++ global_setup_no_sudo.sh
@@ -49,5 +49,5 @@ echo "installing avd:"
 #  --device "Nexus S" --name "nexus" --abi "google_apis/x86" >/dev/null
   #--device "Nexus S" --name "nexus" --abi "default/x86" --skin WVGA800
 
-$ANDROID_HOME/tools/binavdmanager create avd -n nexus -k "system-images;android-25;google_apis;x86"
+$ANDROID_HOME/tools/bin/avdmanager create avd -n nexus -k "system-images;android-25;google_apis;x86"
 /usr/local/lib/android/sdk/tools/emulator -list-avds
