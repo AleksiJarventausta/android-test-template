8e4b3378e493e08417703eebac4ec58463ed1b7a
AleksiJarventausta <aksu.j@hotmail.com>
2021-03-05 11:42:16 +0200

    cleanup

--- global_setup_no_sudo.sh
+++ global_setup_no_sudo.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
+export ANDROID_HOME=/users/runner/Library/Android/sdk
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home
 
-cp test*.js ~/
-cp wdio.conf.js ~/
-cp application.apk ~/
+#cp test*.js ~/
+#cp wdio.conf.js ~/
+#cp application.apk ~/
 
 #Chmod next sh file(s)
 chmod u+x ./run.sh
 chmod u+x ./global_setup.sh
 
 #Appium
-cd
 npm init -y
 npm install --silent -y \
 	appium
@@ -22,61 +23,11 @@ npm install --silent -y --save-dev \
 	@wdio/appium-service \
 	@wdio/local-runner \
 	@wdio/mocha-framework
-#Android sdk
 
-#export AVD_VERSION=25
-#export ANDROID_BUILD_TOOLS_VERSION=25.0.3 
-#export SDK_VERSION=25.2.3
-#mkdir -p $ANDROID_HOME
-#cd $ANDROID_HOME
-#wget -q -O tools.zip https://dl.google.com/android/repository/tools_r${SDK_VERSION}-linux.zip > /dev/null && \
-#    unzip tools.zip > /dev/null && rm tools.zip > /dev/null && \
-#    chmod a+x -R $ANDROID_HOME 
-#
-#
-#echo "updating..."
-#echo y | ${ANDROID_HOME}/tools/android -s update sdk -a -u -t platform-tools,build-tools > /dev/null
-#
-#
-#echo "updating again..."
-#echo y | ${ANDROID_HOME}/tools/android -s update sdk --all --force --no-ui --filter android-$AVD_VERSION,sys-img-x86-google_apis-$AVD_VERSION > /dev/null 
-#
-#export JAVA_HOME=/usr/lib/jvm/adoptopenjdk-8-hotspot-amd64
-
-#echo "installing avd:"
-#echo y | ${ANDROID_HOME}/tools/android -s create avd --force --name android-${AVD_VERSION} \
-#  --device "Nexus S" --name "nexus" --abi "google_apis/x86" >/dev/null
-  #--device "Nexus S" --name "nexus" --abi "default/x86" --skin WVGA800
-
-#$ANDROID_HOME/tools/bin/sdkmanager "system-images;android-27;google_apis;x86" >/dev/null
-#yes | $ANDROID_HOME/tools/bin/sdkmanager --licenses
-
-#echo no | $ANDROID_HOME/tools/bin/avdmanager create avd -n nexus -k "system-images;android-27;google_apis;x86" >/dev/null
-
-#echo "android devices:"
-#/usr/local/lib/android/sdk/tools/emulator -list-avds
-#echo "############################################ END OF SETUP ###########################################################"
-
-#cd
-#echo "avds:"
-
-echo "env:"
-export ANDROID_HOME=/users/runner/Library/Android/sdk
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home
-echo "setting up sdk..."
 #$ANDROID_HOME/tools/bin/sdkmanager --install 'build-tools;30.0.3' platform-tools 'platforms;android-29' > /dev/null
 #$ANDROID_HOME/tools/bin/sdkmanager --install emulator > /dev/null
 $ANDROID_HOME/tools/bin/sdkmanager --install 'system-images;android-29;default;x86' > /dev/null
 echo "installing avd..."
 echo no | $ANDROID_HOME/tools/bin/avdmanager create avd --force -n test --abi 'default/x86' --package 'system-images;android-29;default;x86'
 echo "Everything installed, starting emulator...."
-$ANDROID_HOME/emulator/emulator -avd test -no-window -gpu swiftshader_indirect -no-snapshot -noaudio -no-boot-anim &
-$ANDROID_HOME/platform-tools/adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed) ]]; do sleep 1; done;'
 echo "############################################ END OF SETUP ###########################################################"
-cd 
-#output=''
-#while [[ ${output:0:7} != 'stopped' ]]; do
-#  output=`$ANDROID_HOME/platform-tools/adb shell getprop init.svc.bootanim`
-#  sleep 1
-#done
-
--- run.sh
+++ run.sh
@@ -1,26 +1,9 @@
 #!/bin/bash
 
-# The uploaded user files are always in /submission/user
-# and named identically to config.yaml regardless of the uploaded file names.
-
-# The mount directory from config.yaml is in /exercise.
-# Append the required support files to test user solution.
-
-# "capture" etc description in https://github.com/apluslms/grading-base
 export ANDROID_HOME=/users/runner/Library/Android/sdk
 export JAVA_HOME=/Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home
-#export PATH=$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools:$ANDROID_HOME/build-tools
-#export LD_LIBRARY_PATH=$ANDROID_HOME/emulator/lib64
-#cp $STUDENT/application.apk /home/codegrade/application.apk
-cd 
-#output=''
-#while [[ ${output:0:7} != 'stopped' ]]; do
-#  output=`$ANDROID_HOME/platform-tools/adb shell getprop init.svc.bootanim`
-#  sleep 1
-#done
 
 
+$ANDROID_HOME/platform-tools/adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed) ]]; do sleep 1; done;'
 ./node_modules/.bin/wdio 
 $ANDROID_HOME/platform-tools/adb -s emulator-5554 emu kill
-#kill $server_pid
-#wait $server_pid 2>/dev/null
